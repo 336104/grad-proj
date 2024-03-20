b52e7876271621fecac6e9af7da106d4118857c9
lmx <3361040032@qq.com>
2024-03-20 14:59:59 +0800

    compact

--- models/border_model.py
+++ models/border_model.py
@@ -18,7 +18,8 @@ def decode_labels(labels):
     for i in range(1, len(labels)):
         if labels[i] != labels[i - 1]:
             if labels[i - 1] == 0 or labels[i - 1] == 2:
-                entities.add((start, i))
+                if i - start != 1:
+                    entities.add((start, i))
             if labels[i] == 0 or labels[i] == 2:
                 start = i
     return entities
@@ -44,14 +45,16 @@ model = AutoModelForTokenClassification.from_pretrained(
     NERBertConfig.checkpoint, num_labels=3
 )
 training_args = TrainingArguments(
-    output_dir="mymodel",
+    output_dir="NERBorder",
     learning_rate=2e-5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
-    num_train_epochs=2,
+    num_train_epochs=20,
     weight_decay=0.01,
     evaluation_strategy="epoch",
     save_strategy="epoch",
+    load_best_model_at_end=True,
+    save_total_limit=4,
     logging_steps=100,
 )
 
