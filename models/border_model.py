075e36f55735b65c5b8973c2fa779061464a1896
lmx <3361040032@qq.com>
2024-03-20 19:10:05 +0800

    push to hub

--- models/border_model.py
+++ models/border_model.py
@@ -45,17 +45,17 @@ model = AutoModelForTokenClassification.from_pretrained(
     NERBertConfig.checkpoint, num_labels=3
 )
 training_args = TrainingArguments(
-    output_dir="NERBorder",
+    output_dir="cache/NERBorder",
     learning_rate=2e-5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
-    num_train_epochs=20,
+    num_train_epochs=1,
     weight_decay=0.01,
     evaluation_strategy="epoch",
     save_strategy="epoch",
     load_best_model_at_end=True,
     save_total_limit=4,
-    logging_steps=100,
+    logging_steps=100
 )
 
 trainer = Trainer(
@@ -69,3 +69,4 @@ trainer = Trainer(
 )
 
 trainer.train()
+trainer.push_to_hub()
